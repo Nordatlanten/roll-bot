381e5682d2691fcfa6f03324f977acdb177a873b
Atlan Talebi <gevaliatlaniskt@gmail.com>
2023-03-26 06:35:02 +0200

    Working roll command implemented

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # roll-bot
 A Discord.js-bot for rolling numbers, cause why not make it myself.
 
+Likely to add more commands just for the cheap (monetarily free) entertainment.
+
+---
+
 $ npm i
 to install
 
-Private message me for config file credentials (token and what not)
+Private message me for config file credentials (the token and what not).
+
--- commands/rolls/roll.js
+++ commands/rolls/roll.js
@@ -3,8 +3,22 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('roll')
-		.setDescription('Roll between 1 and 1005'),
+		.setDescription('Roll between a first and second number, 0-9999 at lowest and highest.')
+		.addStringOption(option =>
+			option.setName('input')
+				.setDescription('Roll interval')
+				.setRequired(true)),
 	async execute(interaction) {
-		return interaction.reply(`You get number ${Math.floor(Math.random() * (1005 - 1 + 1) + 1)}`);
+		const input = interaction.options.getString('input') ?? 'No input provided';
+		console.log(/\d{1,3}(-)\d{1,3}/.test(input));
+
+		if (/^\d{1,4}(-)\d{1,4}$/.test(input)) {
+			const values = input.split('-');
+			//	Logs the number so I can confirm this stuff locally in my console. We don't want none of those suspicious rolls, am I right?
+			console.log(values[0], values[1]);
+			await interaction.reply(`You get number ${Math.floor(Math.random() * (parseInt(values[1]) - parseInt(values[0])) + parseInt(values[0]))}`);
+		} else {
+			await interaction.reply('Please set the numbers from 0 and up to 9999 (maximum 4 digits)');
+		}
 	},
 };
